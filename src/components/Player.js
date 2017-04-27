f9e010ff042e18d41616406919655f7d5b237ce3
TinaHeiligers <tina.heiligers@gmail.com>
2017-04-27 17:38:47 -0400

    minor change

---
+++ src/components/Player.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Player extends React.Component {
+  render() {
+    return(
+      <ul>
+        <li key={player.name} className="players">
+          <span>{player.name}</span>
+          <span>Players matched card length goes here</span>
+        </li>
+      </ul>
+    )
+  }
+}
+export default Player;
